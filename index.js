4f35a59aafded02b3e64108222fffc2f52e60881
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-28 13:17:04 -0400

    getting NASA's picture of the day

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
-
+const axios = require('axios');
+require('dotenv').config();
+require('ejs');
 const app = express();
 
+app.set('view engine', 'ejs');
 
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
+        .then(data => {
+            console.log(data.data.hdurl);
+        })
+        .catch(err => console.log(err))
+})
 
 const port = process.env.PORT || 8080;
 
