a5bbde3347c43e380748d63bf3cc513455df5465
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-29 12:53:45 -0400

    fixing errors and worning like class to className

--- src/App.js
+++ src/App.js
@@ -1,63 +1,66 @@
-import react, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
 
     const [user, setUser] = useState(false);
     const [data, setData] = useState();
 
+
+
     return (
         <div className="App">
-            <h1 class="header">NASA: Image Of The Day</h1>
+            <h1 className="header">NASA: Image Of The Day</h1>
 
-            <div class="forms-container">
+            <div className="forms-container">
 
                 {!user &&
                     <>
                         <a href="/googleauth">Login With Google</a>
 
-                        <div class="registerForm">
+                        <div className="registerForm">
                             <h2>Register</h2>
                             <form action="/register" method="POST">
-                                <div class="input-group flex-nowrap">
-                                    <span class="input-group-text" id="addon-wrapping">@</span>
-                                    <input type="text" class="form-control" name="userName" placeholder="userName here"
+                                <div className="input-group flex-nowrap">
+                                    <span className="input-group-text" id="addon-wrapping">@</span>
+                                    <input type="text" className="form-control" name="userName" placeholder="userName here"
                                         aria-label="Username" aria-describedby="addon-wrapping"/>
                                 </div>
-                                <p class="required">Required ^</p>
+                                <p className="required">Required ^</p>
 
-                                <div class="input-group flex-nowrap">
-                                    <span class="input-group-text" id="addon-wrapping">Password</span>
-                                    <input type="text" class="form-control" name="password" placeholder="password here"
+                                <div className="input-group flex-nowrap">
+                                    <span className="input-group-text" id="addon-wrapping">Password</span>
+                                    <input type="text" className="form-control" name="password" placeholder="password here"
                                         aria-describedby="addon-wrapping"/>
                                 </div>
-                                <p class="required">Required ^</p>
+                                <p className="required">Required ^</p>
 
-                                <button type="submit" class="btn btn-primary">Submit</button>
+                                <button type="submit" className="btn btn-primary">Submit</button>
                             </form>
                         </div>
                     </>
                 }
 
                 {!user &&
-                    <div class="loginForm">
+                    <div className="loginForm">
                         <h2>Login</h2>
                         <form action="/login" method="POST">
-                            <div class="input-group flex-nowrap">
-                                <span class="input-group-text" id="addon-wrapping">@</span>
-                                <input type="text" class="form-control" name="userName" placeholder="userName here"
+                            <div className="input-group flex-nowrap">
+                                <span className="input-group-text" id="addon-wrapping">@</span>
+                                <input type="text" className="form-control" name="userName" placeholder="userName here"
                                     aria-label="Username" aria-describedby="addon-wrapping"/>
                             </div>
-                            <p class="required">Required ^</p>
+                            <p className="required">Required ^</p>
 
-                            <div class="input-group flex-nowrap">
-                                <span class="input-group-text" id="addon-wrapping">Password</span>
-                                <input type="text" class="form-control" name="password" placeholder="password here"
+                            <div className="input-group flex-nowrap">
+                                <span className="input-group-text" id="addon-wrapping">Password</span>
+                                <input type="text" className="form-control" name="password" placeholder="password here"
                                     aria-describedby="addon-wrapping" />
                             </div>
-                            <p class="required">Required ^</p>
+                            <p className="required">Required ^</p>
 
-                            <button type="submit" class="btn btn-primary">Submit</button>
+                            <button type="submit" className="btn btn-primary">Submit</button>
                         </form>
                     </div>
                 }
@@ -67,9 +70,9 @@ function App() {
                 <div className="user-container">
                     <h1>Hello {user.userName}</h1>
                     <form action="/logout" method="POST">
-                        <button type="submit" class="btn-danger">Log Out</button>
+                        <button type="submit" className="btn-danger">Log Out</button>
                     </form>
-                    <img src={data.hdurl} />
+                    <img src={data.hdurl} alt='NASAs picture of the day'/>
                 </div>
             }
         </div>
