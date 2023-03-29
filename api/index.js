3a3e765feadf0a271ed80dc611881474c6c39cf5
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-28 20:04:46 -0400

    trying to deploy

--- api/index.js
+++ api/index.js
@@ -27,7 +27,7 @@ User.find()
 
 app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(flash());
