5466de2e29fcad6d364f4033954e6bcd45bf2968
BaseBenjamin13 <benmorgiewicz@gmail.com>
2023-03-29 19:21:06 -0400

    deploying

--- api/index.js
+++ api/index.js
@@ -9,7 +9,7 @@ const path = require('path');
 const bcrypt = require('bcrypt');
 const passport = require('passport');
 const flash = require('express-flash');
-const session = require('express-session');
+const session = require('cookie-session');
 const initializePassport = require('./passport-config');
 initializePassport(
     passport,
